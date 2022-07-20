751418e46e2c45d415eb4b491197cf35ec460670
DontEatThemCookies <davidcostell44@gmail.com>
2022-07-20 09:13:22 +0800

    version 1.2 of scriptsmgr

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # David Costell's Scripts Manager
 Personal pet project with the goal of expanding my Bash knowledge. Not something with real-world applications or anything like that.
-It's exactly what its name describes. Full documentation is in doc.txt
+It's exactly what its name describes - manages your scripts. Full documentation is in doc.txt
 
-Tested on Raspberry Pi OS (based on Debian)
+Tested on Raspberry Pi OS (Debian-based)
+Analyzed with `shellcheck`
 
 ## Install
 ```sh
 curl -fsSL https://raw.githubusercontent.com/DontEatThemCookies/scriptsmgr/main/scriptsmgr.sh > scriptsmgr.sh
 ```
+Configure to your liking.
 If you wish, you may also do the same with doc.txt
-
--- scriptsmgr.sh
+++ scriptsmgr.sh
@@ -1,15 +1,17 @@
+#!/bin/sh
 # David Costell's Scripts Manager
-# v1.1 [07/18/22]
+# v1.2 [07/20/22]
 
 # CONFIG
 SCRIPTS_DIR="/home/$(whoami)/scripts" # The directory containing your scripts
 PREF_EDITOR="nano" # Your preferred editor (e.g. vi, nano)
 PREF_SHELL="bash" # Your preferred editor (e.g. bash, zsh)
 NO_ERR_MSG=0 # If this is set to a non-zero number errormsg() won't output anything
+CURL_FLAGS="-fsSL" # Flags to run curl with (e.g. -fsSL)
 # CONFIG END
 
 errormsg() {
-	[ $NO_ERR_MSG -ne 0 ] || (printf "An error has occurred! Last program exited with status $1\n"; exit $?)
+	[ $NO_ERR_MSG -eq 0 ] && (printf "An error may have just occurred. Last exit status was %s\n" "$?"; exit $?)
 }
 
 type curl > /dev/null 2>&1 && CURL_PRESENT="true" # Check for curl
@@ -17,26 +19,26 @@ type curl > /dev/null 2>&1 && CURL_PRESENT="true" # Check for curl
 case $1 in # parse $1 (action argument)
 	"run")
 		# run [$2: file]
-		$PREF_SHELL $SCRIPTS_DIR/$2.sh || errormsg $?
+		$PREF_SHELL "$SCRIPTS_DIR"/"$2".sh || errormsg $?
 	;;
 	"runweb")
 		# runweb [$2: URL]
 		if [ -z $CURL_PRESENT ]; then
 			printf "curl is not installed\n"; exit
 		else
-			curl -fsSL $2 | $PREF_SHELL || errormsg $?
+			printf "Are you sure? (Y/N) "; read -r ANS
+			[ "$ANS" = "Y" ] || [ "$ANS" = "y" ] && curl $CURL_FLAGS "$2" | $PREF_SHELL || errormsg $?
 		fi
 	;;
 	"edit")
 		# edit [$2: file]
-		$PREF_EDITOR $SCRIPTS_DIR/$2.sh || errormsg $?
+		$PREF_EDITOR "$SCRIPTS_DIR"/"$2".sh || errormsg $?
 	;;
 	"about")
-		printf "Scripts Manager v1.1 [07/18/22]\nby David Costell\n"
+		printf "Scripts Manager v1.2 [07/20/22]\nby David Costell\n"
 	;;
 	*)
 		# when [action] is none of the above (invalid)
 		printf "Usage: scriptsmgr [action] {additional arguments}\nFull documentation can be found in doc.txt\n"
 	;;
 esac
-
